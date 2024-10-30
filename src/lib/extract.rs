4fd5c07e2568d71fe209b630113c5a6c4ae79312
Nruta <nruta.choudhari@duke.edu>
2024-10-30 18:57:01 -0400

    edits

--- src/lib/extract.rs
+++ src/lib/extract.rs
@@ -1,19 +1,18 @@
-// src/library/extract.rs
 use reqwest::blocking::get;
 use std::fs::File;
-use std::io::copy;
-use std::path::Path;
+use std::io::{copy, Cursor};
 
 pub fn extract(data: &str, file_path: &str) -> Result<String, Box<dyn std::error::Error>> {
     // Send a GET request
     let response = get(data)?;
     
     // Create a file to save the content
-    let mut file = File::create(Path::new(file_path))?;
+    let mut file = File::create(file_path)?;
     
-    // Write the content to the file
-    copy(&mut response.take(usize::MAX as u64), &mut file)?;
+    // Read the response body and write it to the file
+    let response_body = response.bytes()?;
+    let mut cursor =  Cursor::new(response_body); // Wrapping the bytes in a Cursor
+    copy(&mut cursor, &mut file)?;
 
-    println!("File downloaded to: {}", file_path);
     Ok(file_path.to_string())
 }
